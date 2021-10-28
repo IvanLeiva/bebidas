8f970c050bdfc6f44d6199f5110a9374a42d98cd
unknown <ivanleiva96@gmail.com>
2021-10-28 12:11:52 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,67 @@
+document.addEventListener('DOMContentLoaded', e => { fetchData() });
+
+
+
+const fetchData = async () => {
+    const res = await fetch('data.json');
+    const data = await res.json()
+
+    pintarProductos(data);
+
+}
+
+
+const contenedorElementos = document.querySelector("#contenedorElementos")
+
+const pintarProductos = (data) => {
+
+
+    const template = document.querySelector('#template-producto').content;
+    const fragment = document.createDocumentFragment()
+
+
+    data.forEach(producto => {
+
+
+        template.querySelector("img").setAttribute("src", producto.imgUrl)
+        template.querySelector("#precioUnidad").textContent = "$" + producto.precioUnidad;
+
+        if (producto.envace == "lata") {
+
+            template.querySelector("p").textContent = "Pack: $" + producto.precioX6;
+            //  template.querySelector("p").dataset.id = producto.id
+        } if (producto.envace == "porron") {
+            console.log(producto.precioX3);
+            template.querySelector("p").textContent = "Por 3 ud: $" + producto.precioX3;
+
+
+        }
+
+
+        if (producto.tipo == "gaseosa") {
+
+            template.querySelector("p").textContent = "Pack 6 ud: $" + producto.precioX6;
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        template.querySelector("h5").textContent = producto.title;
+
+        const clone = template.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+
+    contenedorElementos.appendChild(fragment);
+
+}
